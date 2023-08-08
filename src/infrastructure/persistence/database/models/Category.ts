c204b7c3311e9f854997da78b09fa794721c9690
Jelle Baele <jellebaele@hotmail.com>
2023-08-08 21:56:33 +0200

    Adjust model in order to insert self generated ids

--- src/infrastructure/persistence/database/models/Category.ts
+++ src/infrastructure/persistence/database/models/Category.ts
@@ -1,16 +1,18 @@
 import { model, Schema } from 'mongoose';
 
 export interface CategoryDocument extends Document {
+  _id: string;
   name: string;
   description: string;
 }
 
 const CategorySchema = new Schema<CategoryDocument>(
   {
+    _id: { type: String },
     name: { type: String, required: true, unique: true },
     description: { type: String, required: false, default: '' },
   },
-  { timestamps: true }
+  { timestamps: true, _id: false }
 );
 
 export const CategoryModel = model<CategoryDocument>(
