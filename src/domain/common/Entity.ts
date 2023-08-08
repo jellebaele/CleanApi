544f0c47187bd0177ce36b8372f4dc761d2a60d0
Jelle Baele <jellebaele@hotmail.com>
2023-08-08 21:57:20 +0200

    Return the id instead of object UniqueEntityId

--- src/domain/common/Entity.ts
+++ src/domain/common/Entity.ts
@@ -2,18 +2,18 @@ import { v4 as uuidv4 } from 'uuid';
 import { UniqueEntityId } from './UniqueEntityId';
 
 abstract class Entity<T> {
-  protected readonly _id: UniqueEntityId;
+  protected readonly uniqueEntityId: UniqueEntityId;
   public readonly props: T;
   // DateCreated
   // ...
 
   constructor(props: T, id?: UniqueEntityId) {
     this.props = props;
-    this._id = id ? id : new UniqueEntityId(uuidv4());
+    this.uniqueEntityId = id ? id : new UniqueEntityId(uuidv4());
   }
 
-  public get id(): UniqueEntityId {
-    return this._id;
+  public get id(): string | number {
+    return this.uniqueEntityId._id;
   }
 }
 
