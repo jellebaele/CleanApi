0426667e508ec0af40d79d80a549b8533cbf5307
Jelle Baele <jellebaele@hotmail.com>
2023-08-08 22:28:47 +0200

    Add InternalErrorHandler as middleware to express server

--- src/api/config/server.ts
+++ src/api/config/server.ts
@@ -1,11 +1,14 @@
 import express, { Express } from 'express';
 import { v1Router } from '../routes';
+import { internalErrorHandler } from '../middleware/errorHandlerMiddleware';
 
 export const setupServer = (): Express => {
   const app = express();
 
   app.use(express.json());
   app.use(v1Router);
+
+  app.use(internalErrorHandler);
 
   return app;
 };
